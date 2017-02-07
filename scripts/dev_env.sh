6fb262a987880cad1b1e02b6c4b37bd7b27773f4
Matthias Richter <matthias.richter@identpro.de>
2017-02-07 10:18:12 +0100

    install npm globals w/o sudo

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -1,14 +1,19 @@
 #!/bin/zsh -eux
 
 # install tmuxinator
-gem install tmuxinator
+#gem install tmuxinator
 
 # start elasticsearch ffs
-sudo systemctl start elasticsearch
+# sudo systemctl start elasticsearch
 
-cd /home/vagrant/development/f/finderbox
-bundle install
-echo rake db:insert-test-data
+# cd /home/vagrant/development/finderbox/finderbox
+# bundle install
+# echo rake db:insert-test-data
 
-tmuxinator start finderbox
-tmux attach
+#tmuxinator start finderbox
+#tmux attach
+
+mkdir "${HOME}/.npm-packages"
+
+echo prefix=${HOME}/.npm-packages > ${HOME}/.npmrc
+
